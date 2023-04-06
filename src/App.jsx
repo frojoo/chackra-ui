4fb99685cb742c861a4fe933e435ad1126c315f1
frojoo <wndyd29605@gmail.com>
2023-04-06 11:07:01 +0900

    Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import { Button, ChakraProvider, useDisclosure } from "@chakra-ui/react";
+import {
+  Button,
+  ChakraProvider,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 function App() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <ChakraProvider>
-      <Button>Open</Button>
+      <Flex minH="100vh" justifyContent="center" alignItems="center">
+        <Button onClick={onOpen}>Open</Button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>What's Up!</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>안녕하세요 저는 블록체인 개발자입니다.</ModalBody>
+            <ModalFooter>Designed by, JY</ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Flex>
     </ChakraProvider>
   );
 }
